8d2ec25d95e31c8c51d6f3e446bbdaf29e0e7378
Saksham Jain <71127462+saksham-jain1@users.noreply.github.com>
2022-10-07 16:38:51 +0530

    Update Greeting.js

--- src/Components/Greeting.js
+++ src/Components/Greeting.js
@@ -17,7 +17,7 @@ const Greeting = () => {
       <Heading fontSize="3rem">
         Hey Visitors <span className="waving">👋🏻</span>
       </Heading>
-      <Text p="2rem" textAlign="justify" m="1rem" fontSize="1.6rem">I am a final year B.Tech Student who is placed in one of the leading company Principal Global with a package of 8 Lakhs with dheela saksham jain dhar and bechan prakhar jain aashta and one unplaced abhishek and naman waiting for placement yet. This my portfollio website you can explore more about me on this.</Text>
+      <Text p="2rem" textAlign="justify" m="1rem" fontSize="1.6rem">I am a final year B.Tech Student. This my portfollio website you can explore more about me on this.</Text>
       <a href={Resume} download="Saksham's Resume">
         <Button mt="1rem" colorScheme="green" p="1.4rem" fontSize="1.6rem">
           Download Resume
